d74f2e2ce827adbc414b2e5e849314c339beed34
1005281342@qq.com <1005281342@qq.com>
2019-06-10 17:55:51 +0800

    update

--- day_5/main/main.go
+++ day_5/main/main.go
@@ -1,8 +1,18 @@
 package main
 
-import "golang_learn/day_5/slack"
+import (
+	"fmt"
+	"golang_learn/day_5/slack"
+)
 
 func main() {
 
 	slack.Test_1()
+
+	var ch chan int
+	ch = make(chan int) // 如果不初始化会怎么样 https://github.com/developer-learning/reading-go/issues/392
+	go func() {
+		ch <- 1
+	}()
+	fmt.Println(<-ch)
 }
