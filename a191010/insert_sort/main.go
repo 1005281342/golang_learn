5d914ce60a878076129003bec8365b284aaba169
oyjx <1005281342@qq.com>
2019-10-11 00:10:48 +0800

    ADD INSERT-SORT-O(1) CODE

--- a191010/insert_sort/main.go
+++ a191010/insert_sort/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	res := InsertSort([]int{1, 4, 23, 3, 6, 7})
+	res := InsertSortO1([]int{1, 4, 23, 3, 6, 7})
 	fmt.Println(res)
 }
 
@@ -28,4 +28,22 @@ func InsertSort(nums []int) []int {
 		}
 	}
 	return ans
+}
+
+func InsertSortO1(nums []int) []int {
+
+	if len(nums) == 1 {
+		return nums
+	}
+	// 1 9 19 8
+	for i := 1; i < len(nums); i++ {
+		j := i
+		for j > 0 && nums[j] < nums[j-1] {
+			nums[j], nums[j-1] = nums[j-1], nums[j]
+			j--
+		}
+		fmt.Println(nums)
+	}
+
+	return nums
 }
