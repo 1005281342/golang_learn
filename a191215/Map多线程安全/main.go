9b492357b613068806903a0cd919143bd3331e61
oyjx <1005281342@qq.com>
2019-12-15 17:48:09 +0800

    update

---
+++ a191215/Map多线程安全/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "sync"
+
+type SyncMap struct {
+	MyMap map[string]string
+	sync.RWMutex
+}
+
+func main() {
+
+}
+
+func Read() {
+
+}
+
+func writeA() {
+
+}
+
+func writeB() {
+
+}
