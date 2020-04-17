e83ca6d7e1649756152f559b0fd048aff38386dc
oyjx <1005281342@qq.com>
2020-04-17 23:03:16 +0800

    [upd]

--- nsq/consumer/main.go
+++ nsq/consumer/main.go
@@ -1,54 +1,58 @@
-// nsq_producer/main.go
+// nsq_consumer/main.go
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"strings"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/nsqio/go-nsq"
 )
 
-// NSQ Producer Demo
+// NSQ Consumer Demo
 
-var producer *nsq.Producer
+// MyHandler 是一个消费者类型
+type MyHandler struct {
+	Title string
+}
 
-// 初始化生产者
-func initProducer(str string) (err error) {
+// HandleMessage 是需要实现的处理消息的方法
+func (m *MyHandler) HandleMessage(msg *nsq.Message) (err error) {
+	fmt.Printf("%s recv from %v, msg:%v\n", m.Title, msg.NSQDAddress, string(msg.Body))
+	return
+}
+
+// 初始化消费者
+func initConsumer(topic string, channel string, address string) (err error) {
 	config := nsq.NewConfig()
-	producer, err = nsq.NewProducer(str, config)
+	config.LookupdPollInterval = 15 * time.Second
+	c, err := nsq.NewConsumer(topic, channel, config)
 	if err != nil {
-		fmt.Printf("create producer failed, err:%v\n", err)
+		fmt.Printf("create consumer failed, err:%v\n", err)
+		return
+	}
+	consumer := &MyHandler{
+		Title: "125",
+	}
+	c.AddHandler(consumer)
+
+	// if err := c.ConnectToNSQD(address); err != nil { // 直接连NSQD
+	if err := c.ConnectToNSQLookupd(address); err != nil { // 通过lookupd查询
 		return err
 	}
 	return nil
+
 }
 
 func main() {
-	nsqAddress := "203.195.196.216:4150"
-	err := initProducer(nsqAddress)
+	err := initConsumer("topic_demo", "first", "203.195.196.216:4161")
 	if err != nil {
-		fmt.Printf("init producer failed, err:%v\n", err)
+		fmt.Printf("init consumer failed, err:%v\n", err)
 		return
 	}
-
-	reader := bufio.NewReader(os.Stdin) // 从标准输入读取
-	for {
-		data, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Printf("read string from stdin failed, err:%v\n", err)
-			continue
-		}
-		data = strings.TrimSpace(data)
-		if strings.ToUpper(data) == "Q" { // 输入Q退出
-			break
-		}
-		// 向 'topic_demo' publish 数据
-		err = producer.Publish("topic_demo", []byte(data))
-		if err != nil {
-			fmt.Printf("publish msg to nsq failed, err:%v\n", err)
-			continue
-		}
-	}
+	c := make(chan os.Signal)        // 定义一个信号的通道
+	signal.Notify(c, syscall.SIGINT) // 转发键盘中断信号到c
+	<-c                              // 阻塞
 }
--- nsq/producr/main.go
+++ nsq/producr/main.go
@@ -1,58 +1,54 @@
-// nsq_consumer/main.go
+// nsq_producer/main.go
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"os/signal"
-	"syscall"
-	"time"
+	"strings"
 
 	"github.com/nsqio/go-nsq"
 )
 
-// NSQ Consumer Demo
+// NSQ Producer Demo
 
-// MyHandler 是一个消费者类型
-type MyHandler struct {
-	Title string
-}
+var producer *nsq.Producer
 
-// HandleMessage 是需要实现的处理消息的方法
-func (m *MyHandler) HandleMessage(msg *nsq.Message) (err error) {
-	fmt.Printf("%s recv from %v, msg:%v\n", m.Title, msg.NSQDAddress, string(msg.Body))
-	return
-}
-
-// 初始化消费者
-func initConsumer(topic string, channel string, address string) (err error) {
+// 初始化生产者
+func initProducer(str string) (err error) {
 	config := nsq.NewConfig()
-	config.LookupdPollInterval = 15 * time.Second
-	c, err := nsq.NewConsumer(topic, channel, config)
+	producer, err = nsq.NewProducer(str, config)
 	if err != nil {
-		fmt.Printf("create consumer failed, err:%v\n", err)
-		return
-	}
-	consumer := &MyHandler{
-		Title: "125",
-	}
-	c.AddHandler(consumer)
-
-	// if err := c.ConnectToNSQD(address); err != nil { // 直接连NSQD
-	if err := c.ConnectToNSQLookupd(address); err != nil { // 通过lookupd查询
+		fmt.Printf("create producer failed, err:%v\n", err)
 		return err
 	}
 	return nil
-
 }
 
 func main() {
-	err := initConsumer("topic_demo", "first", "203.195.196.216:4161")
+	nsqAddress := "203.195.196.216:4150"
+	err := initProducer(nsqAddress)
 	if err != nil {
-		fmt.Printf("init consumer failed, err:%v\n", err)
+		fmt.Printf("init producer failed, err:%v\n", err)
 		return
 	}
-	c := make(chan os.Signal)        // 定义一个信号的通道
-	signal.Notify(c, syscall.SIGINT) // 转发键盘中断信号到c
-	<-c                              // 阻塞
+
+	reader := bufio.NewReader(os.Stdin) // 从标准输入读取
+	for {
+		data, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Printf("read string from stdin failed, err:%v\n", err)
+			continue
+		}
+		data = strings.TrimSpace(data)
+		if strings.ToUpper(data) == "Q" { // 输入Q退出
+			break
+		}
+		// 向 'topic_demo' publish 数据
+		err = producer.Publish("topic_demo", []byte(data))
+		if err != nil {
+			fmt.Printf("publish msg to nsq failed, err:%v\n", err)
+			continue
+		}
+	}
 }
